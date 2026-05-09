bfa13cb070385bc3a1d15266cfeacf4011d1fbea
Soni <singhbsoni@gmail.com>
2026-05-09 10:28:16 +0530

    My website is finally ready

---
+++ video 5/script.js
@@ -0,0 +1,13 @@
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+function toggleMode() {
+    document.body.classList.toggle("dark");
+    
+    let btn = document.querySelector(".toggle-btn");
+    if (document.body.classList.contains("dark")) {
+        btn.textContent = "☀️ Light Mode";
+    } else {
+        btn.textContent = "🌙 Dark Mode";
+    }
+}
